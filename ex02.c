cb59bd184f42700f8f5df84913a1668aac89e3b6
Peerapon-Sarikan <68011921@kmitl.ac.th>
2025-07-08 14:22:05 +0000

    Your commit message here

---
+++ ex02.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main ()
+{
+    printf("My name is Owen\n");
+    printf("I am %s years old\n", "18");
+    printf("I am %s cm tall\n", "190.8" );
+  
+    return 0;
+}
---
+++ ex06.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main ()
+{
+    printf("Decimal : %d\n", 35);
+    printf("Octal : %d\n", 43);
+    printf("Hexadecimal : %d\n", 23);
+    return 0;
+}
